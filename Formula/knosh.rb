8bc75f4c560da5560e90dbaa9ad0b45d47d4f34c
mmurphy <mmurphy@commonsware.com>
2026-05-25 15:00:12 -0400

    add knosh formula 0.1.0

---
+++ Formula/knosh.rb
@@ -0,0 +1,19 @@
+class Knosh < Formula
+  desc "AI coding agent CLI"
+  homepage "https://knosh.commonsware.com"
+  url "https://knosh.commonsware.com/binaries/0.1.0/knosh-all.jar"
+  sha256 "068fc7e0c41a5cae00d0ef19b96aa3ecbb9ea8cf4f742ee91e90bd89203f4c15"
+  version "0.1.0"
+  license "Apache-2.0"
+
+  depends_on "openjdk@21"
+
+  def install
+    libexec.install "knosh-all.jar"
+    bin.write_jar_script libexec/"knosh-all.jar", "knosh", java_version: "21"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/knosh --version")
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# homebrew-knosh
-Homebrew distribution information for Knosh
+# Knosh
+
+Knosh is an LLM agent for "one-shot" prompts and structured software development activities. While it has a robust
+suite of tools, it specifically excludes any sort of arbitrary shell access (e.g., a Bash tool). It is written in
+Kotlin, and so it is a Kotlin no-shell (Knosh) agent.
+
+For full project documentation, visit [the Knosh site](https://knosh.commonsware.com).
+
+See [CHANGELOG.md](CHANGELOG.md) for a history of notable changes.
